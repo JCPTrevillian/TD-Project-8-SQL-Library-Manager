9b8e5dd951db70f9978ee36d368c75087a73778b
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-07-29 00:12:41 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Home route redirect to the /books route  */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect("/books");
 });
 
 module.exports = router;
